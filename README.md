f59d1f3900bb2b7bf2ebab8c51997e351ba706ad
cAtPile <799818193@qq.com>
2026-03-04 21:45:19 +0800

    2

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 ## recycle_info.msg
 
+## stepper_cmd.msg
+#stepper_cmd.msg
+int32 stepper_a;
+int32 stepper_b;
+int32 stepper_c;
+int32 stepper_d;
+
+const int moveSteps_x1 = stepper_a;   
+const int moveSteps_x2 =stepper_b;
+const int moveSteps_y1 = stepper_c;
+const int moveSteps_y2 = stepper_d;
--- src/bridge.cpp
+++ src/bridge.cpp
@@ -1,3 +1,26 @@
 /**
  * 
  */
+
+//寄存器 vision_pix_x
+//寄存器 vision_pix_y
+//寄存器 vision_gol_x
+//寄存器 vision_gol_y
+
+
+//参数导入
+//视觉比例参数 visual_proportion_x
+//视觉比例参数 visual_proportion_y
+
+
+//读取视觉信息 vision_info.msg
+//vision_pix_x=yolo_pix_x;
+//vision_pix_y=yolo_pix_y;
+//比例
+//vision_gol_x=visual_proportion_x*yolo_pix_x
+//vision_gol_y=visual_proportion_y*yolo_pix_y
+
+
+//转换为电机步数
+
+//发布电机步数
